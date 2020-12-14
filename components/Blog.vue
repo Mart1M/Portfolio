9105f73580377d4951f93525251c531359bfe574
Martin Molcrette <mart1.molcrette@gmail.com>
2020-12-14 23:13:43 +0100

    fix

--- components/Blog.vue
+++ components/Blog.vue
@@ -1,53 +1,58 @@
 <template>
-<div>
-    <div v-if="error">
-      {{ error }}
-    </div>
+  <div>
+    <div v-if="error">{{ error }}</div>
     <ul v-else class="grid grid-cols-3 gap-10 mt-32">
-      
       <li v-for="projet in projets" :key="projet.id">
         <div class="bg-white flex flex-col rounded-t-3xl rounded-b-2xl">
-                            <div class="px-8 py-6 flex flex-col">
-                                    <img v-if="projet.Image" :src="'https://strapi-backend-mart1.herokuapp.com'+projet.Image[0].url" class="rounded-t-2xl" alt="" srcset="">
-                                <div class="px-8 py-6 flex flex-col">
-                                  <h3 class="text-2xl">{{ projet.Titre }}</h3>
-                                  <span class="text-gray-400">{{ projet.Tag }}</span>
-                                </div>
-                                <a class="border-gray-900 uppercase border-2 mt-10 flex justify-center py-4 rounded-full" :href="projet.Lien" target="_blank">View project →</a>
+          <div class="px-8 py-6 flex flex-col">
+            <img
+              v-if="projet.Image"
+              :src="'https://strapi-backend-mart1.herokuapp.com'+projet.Image[0].url"
+              class="rounded-t-2xl"
+              alt
+              srcset
+            />
+            <div class="px-8 py-6 flex flex-col">
+              <h3 class="text-2xl">{{ projet.Titre }}</h3>
+              <span class="text-gray-400">{{ projet.Tag }}</span>
+            </div>
+            <a
+              class="border-gray-900 uppercase border-2 mt-10 flex justify-center py-4 rounded-full"
+              :href="projet.Lien"
+              target="_blank"
+            >View project →</a>
+          </div>
         </div>
-
-        </div>
-
       </li>
-    
     </ul>
-    
-</div>
-    
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-axios.get('https://strapi-backend-mart1.herokuapp.com/projets').then(response => {
-  console.log(response);
-});
+import axios from "axios";
+axios
+  .get("https://strapi-backend-mart1.herokuapp.com/projets")
+  .then((response) => {
+    console.log(response);
+  });
 
 export default {
-  name: 'projects',
-  data(){
-    return{
+  name: "projects",
+  data() {
+    return {
       projets: [],
-      error: null
-    }
+      error: null,
+    };
   },
-  async mounted () {
+  async mounted() {
     try {
-      const response = await axios.get('https://strapi-backend-mart1.herokuapp.com/projets')
-      this.projets = response.data
+      const response = await axios.get(
+        "https://strapi-backend-mart1.herokuapp.com/projets"
+      );
+      this.projets = response.data;
     } catch (error) {
       this.error = error;
     }
-    
-  }
-}
+  },
+};
 </script>
