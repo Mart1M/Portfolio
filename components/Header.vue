4e081486241c229fd55b24fe0d7171acdfb0f098
Martin Molcrette <mart1.molcrette@gmail.com>
2020-12-14 23:31:42 +0100

    add header

--- components/Header.vue
+++ components/Header.vue
@@ -1,23 +1,28 @@
 <template>
-<header class="text-gray-700 body-font">
-  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span class="ml-3 text-xl">tailblocks</span>
-    </a>
-    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a class="mr-5 hover:text-gray-900">First Link</a>
-      <a class="mr-5 hover:text-gray-900">Second Link</a>
-      <a class="mr-5 hover:text-gray-900">Third Link</a>
-      <a class="mr-5 hover:text-gray-900">Fourth Link</a>
-    </nav>
-    <button class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
-  </div>
-</header>
+  <header class="container mx-auto px-4 z-10 grid md:grid-cols-12 gap-2 text-white mt-6 sticky top-6">
+    <div class="col-start-1 col-span-2">
+      <div class="background-blur rounded-full h-12 w-12 flex items-center justify-center">
+        <a href="#body"><img
+            src="./assets/logo.svg"
+            alt=""
+            srcset=""
+          ></a>
+      </div>
+    </div>
+    <div class="col-start-3 col-span-8 flex justify-center items-center">
+      <nav class="menu flex px-10 rounded-full h-12">
+        <ul class="flex flex-row space-x-10 py-3">
+          <li><a href="#myworks">my works</a></li>
+          <li><a href="#aboutme">about me</a></li>
+          <li><a href="#contactme">contact me</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="col-start-11 col-span-2 md:flex justify-end hidden">
+      <div class="background-blur rounded-full flex space-x-6 items-center justify-center px-8">
+        <a href=""><i class="fab fa-dribbble"></i></a>
+        <a href=""><i class="fab fa-behance"></i></a>
+      </div>
+    </div>
+  </header>
 </template>
