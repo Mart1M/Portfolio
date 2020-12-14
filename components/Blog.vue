38e3e9de41d95243c09f29f75143b506e32c5590
Martin Molcrette <mart1.molcrette@gmail.com>
2020-12-14 22:39:15 +0100

    fix

--- components/Blog.vue
+++ components/Blog.vue
@@ -9,13 +9,12 @@
         <div class="bg-white flex flex-col rounded-t-3xl rounded-b-2xl">
                             <div class="px-8 py-6 flex flex-col">
                                 <div v-if="projet.Image">
-                                    <nuxt-image :src="'https://strapi-backend-mart1.herokuapp.com/'+projet.Image[0].url" alt="" class="rounded-t-2xl" />
+                                    <nuxt-image :src="'https://strapi-backend-mart1.herokuapp.com'+projet.Image[0].url" alt="" class="rounded-t-2xl" />
                                     <img class="crop mx-auto" :src="projet.Image.url">
                                 </div>
                                 <div class="px-8 py-6 flex flex-col">
                                 <h3 class="text-2xl">{{ projet.Titre }}</h3>
                                 <span class="text-gray-400">{{ projet.Tag }}</span>
-                                {{projet.Image.id}}
                                 </div>
                                 <a class="border-gray-900 uppercase border-2 mt-10 flex justify-center py-4 rounded-full" :href="projet.Lien" target="_blank">View project →</a>
         </div>
