321cf20ca8b65727eeab9c2f93e34e7fac262668
Martin Molcrette <mart1.molcrette@gmail.com>
2020-12-14 15:51:57 +0100

    First commit

---
+++ components/Blog.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <ul v-else class="grid grid-cols-3 gap-10 mt-32">
+      
+      <li v-for="projet in projets" :key="projet.id">
+        
+        <div class="bg-white flex flex-col rounded-t-3xl rounded-b-2xl">
+                            <div class="px-8 py-6 flex flex-col">
+                                <div v-if="projet.Image">
+                                    <nuxt-image :src="'http://localhost:1337'+projet.Image.url" alt="" class="rounded-t-2xl" />
+                                </div>
+                                <div class="px-8 py-6 flex flex-col">
+                                <h3 class="text-2xl">{{ projet.Titre }}</h3>
+                                <span class="text-gray-400">{{ projet.Tag }}</span>
+                                </div>
+                                <a class="border-gray-900 uppercase border-2 mt-10 flex justify-center py-4 rounded-full" :href="projet.Lien" target="_blank">View project →</a>
+        </div>
+
+        </div>
+
+      </li>
+    
+    </ul>
+    
+</div>
+    
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'projects',
+  data(){
+    return{
+      projets: [],
+      error: null
+    }
+  },
+  async mounted () {
+    try {
+      const response = await axios.get('http://localhost:1337/projets')
+      this.projets = response.data
+    } catch (error) {
+      this.error = error;
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+  <Header />
+  <Blog />
+</div>
+  
+  
+</template>
+
+
+
