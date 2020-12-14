4fd444220fdebe8ef30421a5ae10a1549cdcaf52
Martin Molcrette <mart1.molcrette@gmail.com>
2020-12-14 22:37:29 +0100

    fix

--- components/Blog.vue
+++ components/Blog.vue
@@ -9,7 +9,7 @@
         <div class="bg-white flex flex-col rounded-t-3xl rounded-b-2xl">
                             <div class="px-8 py-6 flex flex-col">
                                 <div v-if="projet.Image">
-                                    <nuxt-image :src="'https://strapi-backend-mart1.herokuapp.com/'+projet.Image.url" alt="" class="rounded-t-2xl" />
+                                    <nuxt-image :src="'https://strapi-backend-mart1.herokuapp.com/'+projet.Image[0].url" alt="" class="rounded-t-2xl" />
                                     <img class="crop mx-auto" :src="projet.Image.url">
                                 </div>
                                 <div class="px-8 py-6 flex flex-col">
